f3903ff73ad2a46ea0d44f7cbb48db47d7007e2d
ScottCRed <ScottC@redpandasoftware.co.za>
2023-02-03 09:57:08 +0200

    Test webpack watch

--- src/test.js
+++ src/test.js
@@ -1,11 +1,5 @@
 import './style.css';
 
 export function testFunc () {
-    const test = document.querySelector('#container');
-    const testDiv = document.createElement('div');
-    testDiv.classList.add('testDiv')
-    testDiv.textContent = 'HI BIH'
-
-    test.appendChild(testDiv);
     console.log('it works bih')
 }
