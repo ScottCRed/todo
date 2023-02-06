ff02552bc7082bd1dcc362f9cbd92801a2f37223
ScottCRed <ScottC@redpandasoftware.co.za>
2023-02-06 10:59:26 +0200

    Update add project and task functions to display form on button Click

--- src/addProject.js
+++ src/addProject.js
@@ -1,5 +1,17 @@
 import './style.css';
 
 export function addProject () {
+    const addP = document.querySelector('.addProject');
+    addP.classList.add('formAppear');
+    submit();
     console.log('it works bih')
+}
+
+function submit () {
+    const submit = document.querySelector('.submit-button');
+    submit.addEventListener('click', () =>{
+        event.preventDefault();
+        const addP = document.querySelector('.addProject');
+        addP.classList.remove('formAppear');
+    })
 }
