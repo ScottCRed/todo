ff12812c1fa9bd482bae5e5e67a616fc1472d836
ScottCRed <ScottC@redpandasoftware.co.za>
2023-02-06 12:45:29 +0200

    Add projects and tasks objects to capture project title and task object

--- src/addProject.js
+++ src/addProject.js
@@ -1,14 +1,23 @@
 import './style.css';
 
-export function addProject () {
+let projects = [];
+
+function addProject () {
     const addP = document.querySelector('.addProject');
     addP.classList.add('formAppear');
-
+    document.getElementById('projectForm').reset();
     console.log('it works bih')
 }
 
-export function submitProject () {
-        event.preventDefault();
-        const addP = document.querySelector('.addProject');
-        addP.classList.remove('formAppear');
+function submitProject () {
+    event.preventDefault();
+    const addP = document.querySelector('.addProject');
+    addP.classList.remove('formAppear');
+
+    const title = projectForm.projectTitle.value
+    projects.push(title);
+    console.log(title);
+    console.log(projects);
 }
+
+export { addProject, submitProject }
--- src/addTask.js
+++ src/addTask.js
@@ -1,12 +1,30 @@
-export function addTask () {
+
+let tasks = [];
+
+class Task{
+    constructor(taskTitle, description, dueDate, priority) {
+    this.taskTitle = form.title.value;
+    this.description = form.description.textContent;
+    this.dueDate = form.date.value;
+    this.priority = form.priority.value;
+    }
+  }
+
+function addTask () {
     const addP = document.querySelector('.addTask');
     addP.classList.add('formAppear');
     
     console.log('task baby')
 }
 
-export function submitTask () {
-        event.preventDefault();
-        const addT = document.querySelector('.addTask');
-        addT.classList.remove('formAppear');
+function submitTask () {
+    event.preventDefault();
+    const addT = document.querySelector('.addTask');
+    addT.classList.remove('formAppear'); 
+
+    const newTask = new Task(this.taskTitle, this.description, this.dueDate, this.priority);
+    tasks.push(newTask);
+    console.log(tasks)
 }
+
+export {tasks, addTask, submitTask}
