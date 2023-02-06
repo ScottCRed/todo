62a49ba68ed53655683b5f5fd46a712b6e30f2f4
ScottCRed <ScottC@redpandasoftware.co.za>
2023-02-06 15:12:39 +0200

    Add renderprojects function to display all projects which have been created

--- src/addProject.js
+++ src/addProject.js
@@ -2,6 +2,7 @@ import './style.css';
 
 let projects = [];
 
+
 function addProject () {
     const addP = document.querySelector('.addProject');
     addP.classList.add('formAppear');
@@ -14,10 +15,29 @@ function submitProject () {
     const addP = document.querySelector('.addProject');
     addP.classList.remove('formAppear');
 
-    const title = projectForm.projectTitle.value
+    const title = projectForm.projectTitle.value;
     projects.push(title);
     console.log(title);
     console.log(projects);
+
+    renderProjects();
+}
+
+function renderProjects () {
+    const projectContainer = document.querySelector('.projectContainer');
+    projectContainer.innerHTML = ''   
+
+    for (let i = 0; i< projects.length; i++) {
+
+        const projectContainer = document.querySelector('.projectContainer');
+        const projectItem = document.createElement('div');
+        projectItem.setAttribute('id', projects.indexOf(i));
+        projectItem.classList.add('projectItem');
+        projectItem.textContent = projects[i];
+        
+    
+        projectContainer.appendChild(projectItem)
+        }
 }
 
 export { addProject, submitProject }
